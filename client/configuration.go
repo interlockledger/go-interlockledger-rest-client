22b22dad5a5ba09c872f4a4c1f9ef9c69e40b9f2
Fabio Jun Takada Chino <fchino@il2.io>
2023-03-11 20:10:59 -0300

    Method client.Configuration.Init() added (#13).

--- client/configuration.go
+++ client/configuration.go
@@ -31,17 +31,38 @@
 package client
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
 	"net/http"
+
+	"github.com/interlockledger/go-interlockledger-rest-client/crypto"
 )
 
 // Configuration of the client.
 type Configuration struct {
-	BasePath      string            `json:"basePath,omitempty"`
-	Host          string            `json:"host,omitempty"`
-	Scheme        string            `json:"scheme,omitempty"`
+	// Base path of the server API.
+	BasePath string `json:"basePath,omitempty"`
+	// The name of the host.
+	Host string `json:"host,omitempty"`
+	// The scheme to be used.
+	Scheme string `json:"scheme,omitempty"`
+	// Set of headers to be sent on all requests.
 	DefaultHeader map[string]string `json:"defaultHeader,omitempty"`
-	UserAgent     string            `json:"userAgent,omitempty"`
-	HTTPClient    *http.Client
+	// The client's user agent.
+	UserAgent string `json:"userAgent,omitempty"`
+	// If true, clients created by this configuration will
+	NoServerVerification bool `json:"noServerVerification"`
+	// Path to the client certificate file (PEM).
+	CertFile string `json:"certFile,omitempty"`
+	// Path to the client key file (PEM).
+	KeyFile string `json:"keyFile,omitempty"`
+	// Path to the client PFX certificate/key file.
+	PFXFile string `json:"pfxFile,omitempty"`
+	// Password of the client PFX certificate/key file.
+	PFXPassword string `json:"pfxPassword,omitempty"`
+	// The client associated with this configuration.
+	HTTPClient *http.Client `json:"-"`
 }
 
 // Creates a new client configuration.
@@ -57,4 +78,89 @@ func NewConfiguration() *Configuration {
 // Adds custom headers to all connections from this client.
 func (c *Configuration) AddDefaultHeader(key string, value string) {
 	c.DefaultHeader[key] = value
+}
+
+/*
+Loads the client certificate required to access the API and sets the
+`http.Client`.
+*/
+func (c *Configuration) SetClientCertificate(certificateFile string, keyFile string) error {
+	return c.SetClientCertificateEx(certificateFile, keyFile, c.NoServerVerification)
+}
+
+/*
+Loads the client certificate required to access the API and sets the
+`http.Client`.
+*/
+func (c *Configuration) SetClientCertificateEx(
+	certificateFile string, keyFile string, noServerVerification bool) error {
+	cert, err := crypto.LoadCertificateWithKey(certificateFile, keyFile)
+	if err != nil {
+		return err
+	}
+	tls.LoadX509KeyPair(certificateFile, keyFile)
+	if err != nil {
+		return err
+	}
+	return c.SetClientCert(cert, noServerVerification)
+}
+
+/*
+Loads the client certificate required to access the API and sets the
+`http.Client`.
+*/
+func (c *Configuration) SetClientCertificatePKCS12(file string, password string) error {
+	return c.SetClientCertificatePKCS12Ex(file, password, c.NoServerVerification)
+}
+
+/*
+Loads the client certificate required to access the API and sets the
+`http.Client`.
+*/
+func (c *Configuration) SetClientCertificatePKCS12Ex(
+	file string, password string, noServerVerification bool) error {
+	cert, err := crypto.LoadCertificateWithKeyFromPKCS12(file, password)
+	if err != nil {
+		return err
+	}
+	return c.SetClientCert(cert, noServerVerification)
+}
+
+/*
+Loads the client certificate required to access the API and sets the
+`http.Client`.
+*/
+func (c *Configuration) SetClientCert(cert tls.Certificate, noServerVerification bool) error {
+	certPool, err := x509.SystemCertPool()
+	if err != nil {
+		return err
+	}
+	tlsConfig := &tls.Config{
+		Certificates:       []tls.Certificate{cert},
+		RootCAs:            certPool,
+		InsecureSkipVerify: noServerVerification,
+	}
+	c.HTTPClient = &http.Client{Transport: &http.Transport{TLSClientConfig: tlsConfig}}
+	return nil
+}
+
+/*
+Initializes the inner HTTPClient using the current parameters.
+
+It will load the client certificate from either PFXFile (preffered) or
+CertFile/KeyFile parameters and enable or disable the server
+verification based on the value of NoServerVerification.
+
+If called more than once, the previos HTTPClient will be replaced by the new one.
+
+If fails if there is no valid client certificate to load.
+*/
+func (c *Configuration) Init() error {
+
+	if c.PFXFile != "" {
+		return c.SetClientCertificatePKCS12Ex(c.PFXFile, c.PFXPassword, c.NoServerVerification)
+	} else if c.CertFile != "" {
+		return c.SetClientCertificateEx(c.CertFile, c.KeyFile, c.NoServerVerification)
+	}
+	return fmt.Errorf("no client certificate set")
 }
